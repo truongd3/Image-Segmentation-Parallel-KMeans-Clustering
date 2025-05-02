118c0a1624199001cd2d09f2f0448d6667766153
mql <qminh279@gmail.com>
2025-05-02 16:09:31 -0400

    change suggestion

--- src/common/image_utils.cpp
+++ src/common/image_utils.cpp
@@ -33,7 +33,7 @@ std::vector<cv::Vec3f> loadImage(const std::string& image_path,
 cv::Mat load_float_image(const std::string& input) {
     cv::Mat img = cv::imread(input);
     if (img.empty()) {
-        std::cerr << "Failed load\n";
+        std::cerr << "Failed to load image from path: " << input << "\n";
     }
     img.convertTo(img, CV_32F, 1.0F / MAX_PIXEL_VALUE);
     return img;
@@ -42,11 +42,12 @@ cv::Mat load_float_image(const std::string& input) {
 std::vector<float> flatten_image(const cv::Mat& img, size_t N) {
     // Flatten pixel data: row-major, channels interleaved
     std::vector<float> h_pixels(N * PIXEL_DIM);
-    for (int y = 0, idx = 0; y < img.rows; ++y) {
-        for (int x = 0; x < img.cols; ++x, ++idx) {
-            const auto& pix = img.at<cv::Vec3f>(y, x);
-            for (int i = 0; i < PIXEL_DIM; i++) {
-                h_pixels[(idx * PIXEL_DIM) + i] = pix[i];
+    for (size_t y = 0, idx = 0; y < static_cast<size_t>(img.rows); ++y) {
+        for (size_t x = 0; x < static_cast<size_t>(img.cols); ++x, ++idx) {
+            const auto& pix
+                = img.at<cv::Vec3f>(static_cast<int>(y), static_cast<int>(x));
+            for (size_t i = 0; i < PIXEL_DIM; i++) {
+                h_pixels[(idx * PIXEL_DIM) + i] = pix[static_cast<int>(i)];
             }
         }
     }
@@ -57,10 +58,10 @@ void produce_image(size_t rows, size_t cols, const std::vector<int>& h_labels,
                    const std::vector<float>& h_centroids,
                    const std::string& out_path) {
     cv::Mat out((int)rows, (int)cols, CV_32FC3);
-    for (int i = 0, idx = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j, ++idx) {
+    for (size_t i = 0, idx = 0; i < rows; ++i) {
+        for (size_t j = 0; j < cols; ++j, ++idx) {
             int k = h_labels[idx];
-            out.at<cv::Vec3f>(i, j)
+            out.at<cv::Vec3f>(static_cast<int>(i), static_cast<int>(j))
                 = cv::Vec3f(h_centroids[(k * PIXEL_DIM) + 0],
                             h_centroids[(k * PIXEL_DIM) + 1],
                             h_centroids[(k * PIXEL_DIM) + 2]);
