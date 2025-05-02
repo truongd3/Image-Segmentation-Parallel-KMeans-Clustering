ea65a5df2aaff00729af1cd8f7a4bcc65821db67
mql <qminh279@gmail.com>
2025-05-02 15:55:42 -0400

    add more image utils

--- src/common/image_utils.cpp
+++ src/common/image_utils.cpp
@@ -1,4 +1,5 @@
 #include "common/image_utils.hpp"
+#include "common/constants.hpp"
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
@@ -27,6 +28,46 @@ std::vector<cv::Vec3f> loadImage(const std::string& image_path,
     }
     img_float = image_float;
     return pixels;
+}
+
+cv::Mat load_float_image(const std::string& input) {
+    cv::Mat img = cv::imread(input);
+    if (img.empty()) {
+        std::cerr << "Failed load\n";
+    }
+    img.convertTo(img, CV_32F, 1.0F / MAX_PIXEL_VALUE);
+    return img;
+}
+
+std::vector<float> flatten_image(const cv::Mat& img, size_t N) {
+    // Flatten pixel data: row-major, channels interleaved
+    std::vector<float> h_pixels(N * PIXEL_DIM);
+    for (int y = 0, idx = 0; y < img.rows; ++y) {
+        for (int x = 0; x < img.cols; ++x, ++idx) {
+            const auto& pix = img.at<cv::Vec3f>(y, x);
+            for (int i = 0; i < PIXEL_DIM; i++) {
+                h_pixels[(idx * PIXEL_DIM) + i] = pix[i];
+            }
+        }
+    }
+    return h_pixels;
+}
+
+void produce_image(size_t rows, size_t cols, const std::vector<int>& h_labels,
+                   const std::vector<float>& h_centroids,
+                   const std::string& out_path) {
+    cv::Mat out((int)rows, (int)cols, CV_32FC3);
+    for (int i = 0, idx = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j, ++idx) {
+            int k = h_labels[idx];
+            out.at<cv::Vec3f>(i, j)
+                = cv::Vec3f(h_centroids[(k * PIXEL_DIM) + 0],
+                            h_centroids[(k * PIXEL_DIM) + 1],
+                            h_centroids[(k * PIXEL_DIM) + 2]);
+        }
+    }
+    out.convertTo(out, CV_8UC3, MAX_PIXEL_VALUE);
+    cv::imwrite(out_path, out);
 }
 
 } // namespace image_utils
