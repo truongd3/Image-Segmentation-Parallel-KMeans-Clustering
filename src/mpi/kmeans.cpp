4c87868f2c19014819fc00b426b28e9f6096b4b4
Truong Dang <ddtblock1903@gmail.com>
2025-05-06 13:14:18 -0400

    add std and remove blank spaces

--- src/mpi/kmeans.cpp
+++ src/mpi/kmeans.cpp
@@ -2,6 +2,8 @@
 #include <mpi.h>
 #include <random>
 
+using namespace std;
+
 namespace mpi {
 
 Kmeans::Kmeans(int num_clusters, int max_iterations, double tol)
@@ -12,8 +14,7 @@ inline double Kmeans::sqDist(const cv::Vec3f& a, const cv::Vec3f& b) {
     return diff.dot(diff);
 }
 
-void Kmeans::fit(const std::vector<cv::Vec3f>& points,
-                 std::vector<int>& labels) {
+void Kmeans::fit(const vector<cv::Vec3f>& points, vector<int>& labels) {
     int rank = 0, num_procs = 1;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
@@ -22,22 +23,21 @@ void Kmeans::fit(const std::vector<cv::Vec3f>& points,
     labels.assign(N, -1);
 
     // Initialize centers on root process
-    centers = std::vector<cv::Vec3f>(k);
+    centers = vector<cv::Vec3f>(k);
     if (rank == 0) {
-        std::mt19937 rng(12345);
-        std::uniform_int_distribution<int> dist(0, N - 1);
-        for (int j = 0; j < k; ++j)
-            centers[j] = points[dist(rng)];
+        mt19937 rng(12345);
+        uniform_int_distribution<int> dist(0, N - 1);
+        for (int j = 0; j < k; ++j) centers[j] = points[dist(rng)];
     }
 
     // Broadcast initial centers
     MPI_Bcast(centers.data(), k * 3, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-    std::vector<cv::Vec3f> new_centers(k);
-    std::vector<cv::Vec3f> local_sums(k);
-    std::vector<cv::Vec3f> global_sums(k);
-    std::vector<int> local_counts(k);
-    std::vector<int> global_counts(k);
+    vector<cv::Vec3f> new_centers(k);
+    vector<cv::Vec3f> local_sums(k);
+    vector<cv::Vec3f> global_sums(k);
+    vector<int> local_counts(k);
+    vector<int> global_counts(k);
 
     for (int iter = 0; iter < max_iters; iter++) {
         for (int j = 0; j < k; ++j) { // Reset accumulators
@@ -62,18 +62,14 @@ void Kmeans::fit(const std::vector<cv::Vec3f>& points,
         }
 
         // Reduce sums and counts across all processes
-        MPI_Allreduce(local_sums.data(), global_sums.data(), k * 3, MPI_FLOAT,
-                      MPI_SUM, MPI_COMM_WORLD);
-        MPI_Allreduce(local_counts.data(), global_counts.data(), k, MPI_INT,
-                      MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(local_sums.data(), global_sums.data(), k * 3, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+        MPI_Allreduce(local_counts.data(), global_counts.data(), k, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
 
         // Compute new centers and track maximum shift
         // double max_shift2 = 0.0;
         for (int j = 0; j < k; ++j) {
-            if (global_counts[j] > 0)
-                new_centers[j] = global_sums[j] * (1.0f / global_counts[j]);
-            else
-                new_centers[j] = centers[j];
+            if (global_counts[j] > 0) new_centers[j] = global_sums[j] * (1.0f / global_counts[j]);
+            else new_centers[j] = centers[j];
 
             // double shift = sqDist(centers[j], new_centers[j]);
             // if (shift > max_shift2) max_shift2 = shift;
@@ -87,12 +83,11 @@ void Kmeans::fit(const std::vector<cv::Vec3f>& points,
     }
 
     // Gather final labels: each process has its subset in labels, others are -1
-    std::vector<int> global_labels(N, -1);
-    MPI_Allreduce(labels.data(), global_labels.data(), N, MPI_INT, MPI_MAX,
-                  MPI_COMM_WORLD);
-    labels = std::move(global_labels);
+    vector<int> global_labels(N, -1);
+    MPI_Allreduce(labels.data(), global_labels.data(), N, MPI_INT, MPI_MAX, MPI_COMM_WORLD);
+    labels = move(global_labels);
 }
 
-const std::vector<cv::Vec3f>& Kmeans::getCenters() const { return centers; }
+const vector<cv::Vec3f>& Kmeans::getCenters() const { return centers; }
 
 } // namespace mpi
